2cf886288b2ace91f126b4be0cee538f2768cfcc
Cainã Motta <caina.tierling@gmail.com>
2026-02-11 13:53:07 -0300

    finalizing user routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -17,8 +17,65 @@ router.post("/register", async (req, res) => {
         return res.status(400).json({error: "Por favor preencha todos os campos"});
     }
     if(passoword != confirmPassword){
-        return res.status(400).json({error: "As senhas não conferem"})
+        return res.status(400).json({error: "As senhas não conferem"});
+    }
+    const emailExists = await User.findOne({email: email});
+
+    if(emailExists){
+        res.status(400).json({error: "Esse email já foi cadastrado"});
+    }
+
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const user = new User({
+        name: name,
+        email: email,
+        password: passwordHash
+    })
+
+    try{
+
+        const newUser = await user.save();
+
+        const token = jwt.sign(
+            {
+                name: newUser.name,
+                id: newUser._id
+            },
+            "nossosecret"
+        );
+
+        res.json({error: null, msg: "Cadastro realizado com sucesso", token: token, userId: newUser._id})
+
+    }catch(error){
+        res.status(400).json({error})
     }
 });
+
+router.post("/login", async (req, res) => {
+    const user = await User.findOne({email: email})
+    if(!user){
+        res.status(400).json({error: "Email inexistente"});
+    }
+
+    const checkPassword = await bcrypt.compare(password, user.password);
+
+    if(!checkPassword){
+        return res.status(400).json({error: "Senha Incorreta"});
+    }
+    
+    const token = jwt.sign(
+            {
+                name: user.name,
+                id: user._id
+            },
+            "nossosecret"
+        );
+
+        res.json({error: null, msg: "Você está autenticado", token: token, userId: user._id})
+    
+})
+
 
 module.exports = router;
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,40 @@
+const express = require("express").Router();
+const bcrypt = require("bcrypt");
+
+const User = require("../models/user");
+
+const verifyToken = require("../helpers/check-token");
+
+const getUserByToken = require("../helpers/get-user-by-token");
+
+router.get("/:id", async(req, verifyToken ,res) => {
+
+    const id = req.params.id;
+
+    try{
+        const user = await User.findOne({_id: id}, {password : 0});
+
+        res.json({error: null, user});
+
+    }catch(error){
+        return res.status(400).json({error:"Usuário inexistente"});
+    }
+
+});
+
+router.put("/", async (req, verifyToken, res) => {
+    const token = req.header("auth-token");
+    const user = await getUserByToken(token);
+    const userReqId = req.body.id;
+    const password = req.body.password;
+    const confirmPassword = req.body.confirmPassword;
+
+    const userId = user._id.toString();
+
+    if(userId != userReqId){
+        res.status(401).json({error: "Acesso Negado"})
+    }
+
+});
+
+module.exports = userRouter;
